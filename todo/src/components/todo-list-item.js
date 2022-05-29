7280cca24ef848733bb817cdfa3cfe7fee646a9b
camarus <rustik73rus@gmail.com>
2022-05-29 15:43:06 +0400

    add css and bootstrap

--- todo/src/components/todo-list-item.js
+++ todo/src/components/todo-list-item.js
@@ -1,7 +1,33 @@
 import React from 'react';
 
-const TodoListItem = () => {
-  return <span>Drink Coffee</span>;
+import './todo-list-item.css';
+
+const TodoListItem = ({ label, important = false }) => {
+
+  const style = {
+    color: important ? 'steelblue' : 'black',
+    fontWeight: important ? 'bold' : 'normal'
+  };
+
+  return (
+    <span className="todo-list-item">
+      <span
+        className="todo-list-item-label"
+        style={style}>
+        {label}
+      </span>
+
+      <button type="button"
+              className="btn btn-outline-success btn-sm float-right">
+        <i className="bi bi-archive-fill" />
+      </button>
+
+      <button type="button"
+              className="btn btn-outline-danger btn-sm float-right">
+        <i className="bi bi-exclamation-lg" />
+      </button>
+    </span>
+  );
 };
 
 export default TodoListItem;
